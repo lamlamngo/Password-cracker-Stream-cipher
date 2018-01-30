cd28a08974e5fd1582f38a8cbfc8b602d78222ca
Lam Ngo <lamngo@Lams-MacBook-Pro.local>
2018-01-29 19:25:19 -0500

    implemented brute force password cracking algorithm

--- crack.py
+++ crack.py
@@ -2,8 +2,46 @@ import string, hashlib, binascii, os, hmac, time
 
 characters = string.printable
 
-def brute_force(pwd):
-    return 0
+def brute_force(pwd, salt):
+    start = time.time()
+    global characters
+    attempt = 0
+    prev = []
+    pw = ""
+    found = False
+    for i in characters:
+        attemp = attempt + 1
+        if passwordcompare(i,pwd,salt):
+            pw = i
+            found = True
+            end = time.time()
+            break
+        else:
+            prev.append(i)
+
+    while not found:
+        temp = []
+        for x in prev:
+            for i in characters:
+                attemp = attempt + 1
+                if passwordcompare(x+i,pwd,salt):
+                    pw = x + i
+                    found = True
+                    end = time.time()
+                    break
+                else:
+                    temp.append(x + i)
+        prev = temp
+
+    if found:
+        print ("password found: ", pw)
+        print ("number of password checked: ", attempt)
+        print ("time elapsed: ", end-start)
+    else:
+        print ("password not found.")
+        print ("number of password checked: ", attemp)
+        print ("time elapsed: ", end-start)
+
 
 def dictionary_attack(pwd, salt):
     start = time.time()
@@ -14,9 +52,7 @@ def dictionary_attack(pwd, salt):
     int count = 0
     for word in lines:
         count = count + 1
-        guess = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000)
-
-        if hmac.compare_digest(guess,binascii.unhexlify(pwd.encode())):
+        if passwordcompare(word,pwd,salt):
             pw = word
             break
     end = time.time()
@@ -28,3 +64,7 @@ def dictionary_attack(pwd, salt):
         print ("password not found.")
         print ("number of password checked: ", count)
         print ("time elapsed: ", end-start)
+
+def passwordcompare(guess,pw,salt):
+    guess = hashlib.pbkdf2_hmac('sha256', guess.encode(), salt, 100000)
+    return hmac.compare_digest(guess,binascii.unhexlify(pwd.encode()))
